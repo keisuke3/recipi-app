0d8325ddc42ef8eb5050b5916f68f091dc793a78
keisuke <ka.9na3fh.h@gmail.com>
2020-10-17 15:41:58 +0900

    モックサーバー等の設定

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,8 +1,18 @@
 import '../css/index.scss';
 import Vue from 'vue';
 import App from '../vue/containers/App.vue';
+import { requestApi } from './utilities/requestApi';
+
+const API_CONF = {
+  method: 'get',
+  url: '/recipes',
+};
 
 new Vue({
   el: '#app',
   render: h => h(App),
 });
+
+(async () => {
+  const recipes = await requestApi(API_CONF);
+})();
